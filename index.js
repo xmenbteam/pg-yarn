09feba094ec81811a82148d299f27ef5f32eb4c0
Sam Parry <67956822+xmenbteam@users.noreply.github.com>
2022-03-09 14:55:35 +0000

    add url to package.json

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const { mkdir, writeFile, readFile, rm } = require("fs/promises");
 const { execSync } = require("child_process");
 
 const zshName = process.argv[1];
+const zshUrl = process.argv[2];
 const callBack = (err, success) => console.log(err ? err : success);
 
-console.log({ zshName });
-
-const projectGenerator = async (projectName, cb) => {
-  const dir = `./${projectName}`;
+const projectGenerator = async (
+  projectName,
+  url = "www.placeholder.com",
+  cb
+) => {
+  const dir = `${__dirname}/${projectName}`;
   const specDir = `${dir}/spec`;
   const readMeHeader = `# ${projectName}`;
   const helloWorld = 'console.log("hello, world!")';
@@ -18,40 +21,51 @@ const projectGenerator = async (projectName, cb) => {
   const gitInit = `cd ${dir} && git init`;
   const installJest = `cd ${dir} && yarn add jest -D`;
 
-  console.log("Writing Dir...");
-  await mkdir(`${dir}`);
+  try {
+    console.log("Writing Dir...");
+    await mkdir(`${dir}`);
 
-  console.log("Writing index.js...");
-  await writeFile(`${dir}/index.js`, helloWorld);
+    console.log("Writing index.js...");
+    await writeFile(`${dir}/index.js`, helloWorld);
 
-  console.log("Initialising project...");
-  execSync(installCommand, { stdio: "ignore" });
+    console.log("Initialising project...");
+    execSync(installCommand, { stdio: "ignore" });
 
-  console.log(".gitignore...");
-  await writeFile(`${dir}/.gitignore`, gitignoreText);
+    console.log(".gitignore...");
+    await writeFile(`${dir}/.gitignore`, gitignoreText);
 
-  console.log("specDir/test.js...");
-  await mkdir(specDir);
-  await writeFile(`${specDir}/index.test.js`, testToDo);
+    console.log("specDir/test.js...");
+    await mkdir(specDir);
+    await writeFile(`${specDir}/index.test.js`, testToDo);
 
-  console.log("Readme...");
-  await writeFile(`${dir}/README.md`, readMeHeader);
+    console.log("Readme...");
+    await writeFile(`${dir}/README.md`, readMeHeader);
 
-  console.log("Git init...");
-  execSync(gitInit, { stdio: "ignore" });
-  await writeFile(`${dir}/.eslintrc.json`, JSON.stringify(eslintText, null, 2));
+    console.log("Git init...");
+    execSync(gitInit, { stdio: "ignore" });
+    await writeFile(
+      `${dir}/.eslintrc.json`,
+      JSON.stringify(eslintText, null, 2)
+    );
 
-  console.log("Installing jest...");
-  execSync(installJest, { stdio: "ignore" });
+    console.log("Installing jest...");
+    execSync(installJest, { stdio: "ignore" });
 
-  console.log("Writing test script");
-  const packageJSON = await readFile(`${dir}/package.json`);
-  const parsedPackage = JSON.parse(packageJSON);
-  newPackage = { ...parsedPackage, scripts: { test: "jest" } };
-  await writeFile(`${dir}/package.json`, JSON.stringify(newPackage, null, 2));
+    console.log("Writing test script");
+    const packageJSON = await readFile(`${dir}/package.json`);
+    const parsedPackage = JSON.parse(packageJSON);
+    newPackage = {
+      ...parsedPackage,
+      repository: url,
+      scripts: { test: "jest" },
+    };
+    await writeFile(`${dir}/package.json`, JSON.stringify(newPackage, null, 2));
 
-  cb(null, "Project built!!");
+    cb(null, "Project built!!");
+  } catch (err) {
+    cb(err, null);
+  }
 };
 
 module.exports = projectGenerator;
-module.exports.init = projectGenerator(zshName, callBack);
+module.exports.pgyarn = projectGenerator(zshName, zshUrl, callBack);
